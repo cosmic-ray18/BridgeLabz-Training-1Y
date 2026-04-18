fc140ecc2f4eca97effcd978d4b55c89ca2497bb
cosmic -ray18 <goyalrishi4431y@gmail.com>
2026-04-18 13:24:17 +0530

    Temporary removal for clean push of codes

--- src/com/gla/PracticeProblem/Streamapi/StreamApiPractice.java
+++
@@ -1,7 +0,0 @@
-package com.gla.PracticeProblem.Streamapi;
-
-public class StreamApiPractice {
-    public static void main (String[] args){
-
-    }
-}
--- src/com/gla/PracticeProblem/Wrapper_classes/Basic_wrapper.java
+++
@@ -1,9 +0,0 @@
-package com.gla.PracticeProblem.Wrapper_classes;
-
-class Basic_wrapper {
-    static Integer i = 5; // Autoboxing ( complier runs valueof() internally in system)
-    public static void main(String[] args) {
-        int I = i; // AutoUnboxing ( complier runs intValue() internally)
-        System.out.println("i = " + I);
-    }
-}
